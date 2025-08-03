1afb9e809df76f1d646969e20c30ea75b08438ba
mohbe1993 <mohamedbehairy1993@yahoo.com>
2025-08-04 02:19:10 +0400

    pages height

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -2,29 +2,37 @@ import { Card, CardActionArea, CardContent, CardMedia, Container, Typography } f
 
 function About() {
     return (
-        <Container sx={{ display: "flex", alignItems: 'center', justifyContent: "flex-start", height: '90vh' }} >
+        <div className={`w-full bg-[url('/imgs/IMG_20250102_190105.jpg')] bg-cover bg-center lg:bg-[center_75%] bg-no-repeat `}>
 
-            <Card sx={{ maxWidth: 345, display: "flex", flexDirection: "column", }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/imgs/360_F_180837604_UyJZNTHPluIJNQJjmTkCpE4XLJ03Zott.jpg"
-                        alt="green iguana"
-                    />
-                    <CardContent>
+            <Container sx={{
+                display: "flex", alignItems: 'center', justifyContent: "flex-start", height: {
+                    xs: 'calc(100vh - 3.5rem)',
+                    md: 'calc(100vh - 4.5rem)',
+                    lg: 'calc(100vh - 4.3rem)',
+                },
+            }} >
 
-                        <Typography gutterBottom variant="h5" component="div">
-                            Mohamed
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            I&apos;m a front-end developer with a focus on building responsive and user-friendly web applications using modern frameworks like Next.js and React. I have experience working with Tailwind CSS, Material UI, and integrating APIs. My projects reflect a strong attention to design, performance, and clean code. I’m passionate about continuous learning, UI/UX improvement, and creating seamless digital experiences.
+                <Card sx={{ maxWidth: 345, display: "flex", flexDirection: "column", }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="/imgs/360_F_180837604_UyJZNTHPluIJNQJjmTkCpE4XLJ03Zott.jpg"
+                            alt="green iguana"
+                        />
+                        <CardContent>
+
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mohamed
+                            </Typography>
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                I&apos;m a front-end developer with a focus on building responsive and user-friendly web applications using modern frameworks like Next.js and React. I have experience working with Tailwind CSS, Material UI, and integrating APIs. My projects reflect a strong attention to design, performance, and clean code. I’m passionate about continuous learning, UI/UX improvement, and creating seamless digital experiences.
 
 
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card></Container>
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card></Container></div>
     )
 }
 
--- app/Contacts/page.tsx
+++ app/Contacts/page.tsx
@@ -10,42 +10,49 @@ function Contact() {
     return (
 
 
+        <div className={`w-full bg-[url('/imgs/contact-us-background-eps-vector-600nw-2491405611.webp')] bg-cover bg-center lg:bg-[center_75%] bg-no-repeat `}>
 
 
-        <Container className='flex items-center justify-center  h-[90vh] '>
-            <Box sx={{ bgcolor: 'inherit', height: '50vh' }} >
+            <Container className='flex items-center justify-center' sx={{
+                height: {
+                    xs: 'calc(100vh - 3.5rem)',
+                    md: 'calc(100vh - 4.5rem)',
+                    lg: 'calc(100vh - 4.3rem)',
+                },
+            }}>
+                <Box sx={{ bgcolor: 'inherit', height: '50vh' }} >
 
-                <CardContent className='flex flex-col gap-5 text-blue-300'>
-                    <Typography variant="h3" component="div">
-                        Let&apos;s get in touch
-                    </Typography>
-                    <hr />
-                    <Typography sx={{ fontSize: 14 }}>
+                    <CardContent className='flex flex-col gap-5 text-blue-300'>
+                        <Typography variant="h3" component="div">
+                            Let&apos;s get in touch
+                        </Typography>
+                        <hr />
+                        <Typography sx={{ fontSize: 14 }}>
 
 
-                        <WhatsAppIcon />
-                        <PhoneAndroidIcon />
+                            <WhatsAppIcon />
+                            <PhoneAndroidIcon />
 
-                    </Typography>
-                    <Typography
-                        variant="h5"
-                        component="a"
-                        href="tel:+971586533278"
-                        sx={{ textDecoration: 'underline', }}
-                    >
-                        +971586533278
-                    </Typography>
-                    <hr />
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            component="a"
+                            href="tel:+971586533278"
+                            sx={{ textDecoration: 'underline', }}
+                        >
+                            +971586533278
+                        </Typography>
+                        <hr />
 
-                    <Typography sx={{ mb: 1.5 }}><EmailIcon /></Typography>
-                    <Typography variant="h5" component="a" href="mailto:mohamedbehairy1993@yahoo.com" sx={{ color: 'inherit', textDecoration: 'underline' }}>
-                        mohamedbehairy1993@yahoo.com
-                    </Typography>
-                </CardContent>
+                        <Typography sx={{ mb: 1.5 }}><EmailIcon /></Typography>
+                        <Typography variant="h5" component="a" href="mailto:mohamedbehairy1993@yahoo.com" sx={{ color: 'inherit', textDecoration: 'underline' }}>
+                            mohamedbehairy1993@yahoo.com
+                        </Typography>
+                    </CardContent>
 
-            </Box>
-        </Container>
-
+                </Box>
+            </Container>
+        </div>
 
 
     )
